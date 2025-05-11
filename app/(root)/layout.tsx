12b7730c2a2568cf41a316af90e74b607249b02c
Diego Valencia <diegovamx@gmail.com>
2025-05-11 15:12:45 -0700

    added interview cards

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,8 +1,20 @@
 import React, { ReactNode } from 'react'
+import Link from 'next/link'
+import Image from 'next/image'
 
-const RootLayout = ({children}: {children: ReactNode}) => {
+const RootLayout = ({children}: {children: ReactNode}) => { 
   return (
-    <div>{children}</div> //render the children components here
+    <div className='root-layout'>
+      <nav> 
+        <Link href="/" className="flex items gap-2">
+          <Image src="/logo.svg" alt="logo" width={38} height={32} />
+          <h2 className="text-primary-100">PrepAI</h2>
+        </Link>
+      </nav>
+
+      {children}
+
+    </div> //render the children components here
   // this is the root layout for the app
     // it will be used to wrap all the pages in the app
   )
