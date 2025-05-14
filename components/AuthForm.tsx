d533b974f8dfd5db746857eb84d38ef67d1a87c0
Diego Valencia <diegovamx@gmail.com>
2025-05-13 23:59:34 -0700

    finished sign in signup code

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -6,14 +6,16 @@ import Link from "next/link";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
+
 import { Button } from "@/components/ui/button";
 import { Form } from "@/components/ui/form";
 import { toast } from "sonner";
 import FormField from "./FormField";
 import { useRouter } from "next/navigation";
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
 import { auth } from "@/firebase/client";
-import { sign } from "crypto";
+import { signUp, signIn } from "@/lib/actions/auth.action";
+
 
 const authFormSchema = (type: FormType) => {
   return z.object({
@@ -49,15 +51,28 @@ const AuthForm = ({ type }: { type: FormType }) => {
           name: name!,
           email: email,
           password: password,
-        });
+        })
         if (!result?.success) {
-          toast.error(result.message);
+          toast.error(result?.message);
           return;
         }
 
         toast.success("Account created successfully. Please sign in.");
         router.push("/sign-in");
       } else {
+        const { email, password } = values;
+        const userCredential = await signInWithEmailAndPassword(auth, email, password);
+
+        const idToken = await userCredential.user.getIdToken()
+
+        if (!idToken) {
+          toast.error("Error signing in. Please try again.");
+          return;
+        }
+        await signIn({
+          email, idToken
+        })
+
         toast.success("Signed in  successfully.");
         router.push("/");
       }
--- lib/actions/auth.action.ts
+++ lib/actions/auth.action.ts
@@ -1,6 +1,9 @@
 "use server";
 
-import { db } from "@/firebase/admin";
+import { db, auth } from "@/firebase/admin";
+import { cookies } from "next/headers";
+
+const WEEK = 60 * 60 * 24 * 7; // 7 days
 
 export async function signUp(params: SignUpParams) {
   const { uid, name, email } = params;
@@ -15,9 +18,13 @@ export async function signUp(params: SignUpParams) {
     }
     await db.collection("users").doc(uid).set({
       name,
-      email
+      email,
     });
-  } catch (e:any) {
+    return {
+      success: true,
+      message: "User signed up successfully. Please sign in.",
+    }
+  } catch (e: any) {
     console.error("Error signing up user:", e);
 
     if (e.code === "auth/email-already-exists") {
@@ -31,4 +38,73 @@ export async function signUp(params: SignUpParams) {
       message: "Error signing up user",
     };
   }
+}
+
+export async function signIn(params: SignInParams) {
+  const { email, idToken } = params;
+  try {
+    const userRecord = await auth.getUserByEmail(email);
+    if (!userRecord) {
+      return {
+        success: false,
+        message: "User does not exist. Please sign up.",
+      };
+    }
+    await setSessionCookie(idToken);
+  } catch (error) {
+    console.error("Error signing in user:", error);
+    return {
+      success: false,
+      message: "Error signing in user",
+    };
+  }
+}
+
+export async function setSessionCookie(idToken: string) {
+  const cookieStore = await cookies();
+
+  const sessionCookie = await auth.createSessionCookie(idToken, {
+    expiresIn: WEEK * 1000, // 7 days
+  });
+
+  cookieStore.set("session", sessionCookie, {
+    maxAge: WEEK * 1000, // 7 days
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    path: "/",
+    sameSite: "lax",
+  });
+}
+
+export async function getCurrentUser() : Promise<User | null> {
+  const cookieStore = await cookies();
+  const sessionCookie = cookieStore.get("session")?.value;
+
+  if (!sessionCookie) {
+    return null;
+  }
+
+  try {
+    const decodedClaims = await auth.verifySessionCookie(sessionCookie, true);
+    const userRecord = await db.collection("users").doc(decodedClaims.uid).get();
+
+    if (!userRecord.exists) {
+      return null;
+    }
+
+    return {
+      ... userRecord.data(),
+      id: userRecord.id,
+
+    } as User;
+  } catch (error) {
+    console.error("Error getting current user:", error);
+    return null;
+  }
+}
+
+export async function isAuthenticated() {
+  const user = await getCurrentUser();
+  
+  return !!user;
 }
