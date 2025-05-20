d0869b7a59b64850bfa9170452b0f9dee9cd81d2
Diego Valencia <diegovamx@gmail.com>
2025-05-20 09:58:20 -0700

    updated authentication

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,20 +1,13 @@
-import {ReactNode} from 'react'
-import { isAuthenticated } from '@/lib/actions/auth.action'
-import { redirect } from 'next/navigation'
+import { ReactNode } from "react";
+import { redirect } from "next/navigation";
 
-const AuthLayout = async ({ children, pathname }: { children: ReactNode, pathname?: string }) => {
-  // Only check authentication for pages that aren't sign-in or sign-up
-  const path = pathname || '';
-  const isAuthPage = path.includes('sign-in') || path.includes('sign-up');
-  
-  if (!isAuthPage) {
-    const isUserAuthenticated = await isAuthenticated();
-    if (!isUserAuthenticated) redirect('/sign-in');
-  }
-  
-  return (
-    <div className='auth-layout'>{children}</div>
-  )
-}
+import { isAuthenticated } from "@/lib/actions/auth.action";
 
-export default AuthLayout
+const AuthLayout = async ({ children }: { children: ReactNode }) => {
+  const isUserAuthenticated = await isAuthenticated();
+  if (isUserAuthenticated) redirect("/");
+
+  return <div className="auth-layout">{children}</div>;
+};
+
+export default AuthLayout;
--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,28 +1,26 @@
-import React, { ReactNode } from 'react'
-import Link from 'next/link'
-import Image from 'next/image'
-import { isAuthenticated } from '@/lib/actions/auth.action'
-import { redirect } from 'next/navigation'
+import Link from "next/link";
+import Image from "next/image";
+import { ReactNode } from "react";
+import { redirect } from "next/navigation";
 
-const RootLayout = async ({children}: {children: ReactNode}) => { 
+import { isAuthenticated } from "@/lib/actions/auth.action";
+
+const Layout = async ({ children }: { children: ReactNode }) => {
   const isUserAuthenticated = await isAuthenticated();
+  if (!isUserAuthenticated) redirect("/sign-in");
 
-  if (!isUserAuthenticated) redirect('/sign-in');
   return (
-    <div className='root-layout'>
-      <nav> 
-        <Link href="/" className="flex items gap-2">
-          <Image src="/logo.svg" alt="logo" width={38} height={32} />
-          <h2 className="text-primary-100">PrepAI</h2>
+    <div className="root-layout">
+      <nav>
+        <Link href="/" className="flex items-center gap-2">
+          <Image src="/logo.svg" alt="MockMate Logo" width={38} height={32} />
+          <h2 className="text-primary-100">PrepWise</h2>
         </Link>
       </nav>
 
       {children}
+    </div>
+  );
+};
 
-    </div> //render the children components here
-  // this is the root layout for the app
-    // it will be used to wrap all the pages in the app
-  )
-}
-
-export default RootLayout
+export default Layout;
